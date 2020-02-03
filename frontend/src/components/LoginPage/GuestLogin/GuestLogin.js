8bb92d8d314da0d36f046cfe3455ae0a0c38c217
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-02-03 23:44:13 +0000

    changing guest login rendering based on user log in state

--- frontend/src/components/LoginPage/GuestLogin/GuestLogin.js
+++ frontend/src/components/LoginPage/GuestLogin/GuestLogin.js
@@ -14,24 +14,35 @@ export default class GuestLogin extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-  }
+  };
 
   render() {
-    return (
-      <form className="guest-login-container" onSubmit={this.handleSubmit}>
-        <input
-          className="guest-name-input"
-          type="text"
-          value={this.state.value}
-          placeholder="...Type your guest name here"
-          onChange={this.handleChange}
-        />
-        <input
-          className="guest-login-button"
-          type="submit"
-          value="Log In as Guest"
-        />
-      </form>
-    );
+    const { userDetails } = this.props;
+
+    if (!userDetails.isSignedIn) {
+      return (
+        <form className="guest-login-container" onSubmit={this.handleSubmit}>
+          <input
+            className="guest-name-input"
+            type="text"
+            value={this.state.value}
+            placeholder="...Type your guest name here"
+            onChange={this.handleChange}
+          />
+          <input
+            className="guest-login-button"
+            type="submit"
+            value="Log In as Guest"
+          />
+        </form>
+      );
+    } else {
+      return (
+        <div className="guest-login-container">
+          <h2 className="guest-name">Hi, {userDetails.userGivenName}</h2>
+          <button className="guest-login-button">Go to Projects</button>
+        </div>
+      );
+    }
   }
 }
