53e6b14c9172893e18d0e34be06e7e66f3c4321f
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-02-23 12:35:24 +0000

    creating task column with simplebar-react

--- frontend/src/components/ProjectPage/TaskColumn/TaskCard/TaskCard.js
+++ frontend/src/components/ProjectPage/TaskColumn/TaskCard/TaskCard.js
@@ -5,7 +5,16 @@ import "./css/task-card.css";
 export const TaskCard = () => {
   return (
     <div className="task-card">
-      <div>TaskCard</div>
+      <div className="task-card__details">
+        <div className="task-card__details__name">Task Name</div>
+        <div className="task-card__details__id">#TaskId</div>
+        <div className="task-card__details__severity" />
+      </div>
+      <div className="task-card__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan
+        lacus vitae semper venenatis. Aenean euismod et erat vel pellentesque.
+        Proin elementum, nisi ac venenatis ultrices.
+      </div>
     </div>
   );
 };
--- frontend/src/components/ProjectPage/TaskColumn/TaskColumn.js
+++ frontend/src/components/ProjectPage/TaskColumn/TaskColumn.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { TaskCard } from "./TaskCard/TaskCard";
+import SimpleBarReact from "simplebar-react";
 
+import "simplebar/src/simplebar.css";
 import "./css/task-column.css";
 
 export const TaskColumn = () => {
   return (
     <div className="task-column">
-      <TaskCard />
+      <SimpleBarReact>
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </SimpleBarReact>
     </div>
   );
 };
