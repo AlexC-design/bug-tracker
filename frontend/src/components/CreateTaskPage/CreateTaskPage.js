64dac56b079b232968330cd587aeb63c4aa89c0b
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-02-29 11:01:11 +0000

    working on task creation page

--- frontend/src/components/CreateTaskPage/CreateTaskPage.js
+++ frontend/src/components/CreateTaskPage/CreateTaskPage.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 
 export default class CreateTaskPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskName: "",
+      taskSeverity: "medium",
+      taskSummary: "",
+      taskDescription: "",
+      environment: "select"
+    };
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
-      <div>
-        <div>CreateTaskPage</div>
-      </div>
+      <form className="task-creation">
+        <div className="task-creation__top">
+          <label>
+            Name:
+            <input
+              name="taskName"
+              type="text"
+              value={this.state.taskName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+        </div>
+      </form>
     );
   }
 }
