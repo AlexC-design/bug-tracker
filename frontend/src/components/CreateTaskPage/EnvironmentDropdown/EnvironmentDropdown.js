0beed9c78ac4b6bba8ed766d6fc705059dd5c099
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-01 17:10:24 +0000

    styling environment dropdown

--- frontend/src/components/CreateTaskPage/EnvironmentDropdown/EnvironmentDropdown.js
+++ frontend/src/components/CreateTaskPage/EnvironmentDropdown/EnvironmentDropdown.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react";
+import dropdownArrow from "../../../assets/svg/dropdown-arrow.svg";
+
+import "./css/environment-dropdown.css";
 
 export const EnvironmentDropdown = () => {
-  const [selected, setSelected] = useState("Selected");
+  const [selected, setSelected] = useState("Select");
   const [expanded, setExpanded] = useState(false);
 
   const options = ["Chrome", "Firefox", "Safari", "Opera", "Edge", "IE", "All"];
 
   return (
-    <div className="environment-dropdown">
-      <div className="environment-dropdown__top">
-        <div className="environment-dropdown__top__selected">{selected}</div>
-        <button className="environment-dropdown__top__button" />
+    <>
+      <label>Environment</label>
+      <div className="environment-dropdown">
+        <div className="environment-dropdown__top">
+          <div className="environment-dropdown__top__selected">{selected}</div>
+          <img
+            src={dropdownArrow}
+            className="environment-dropdown__top__arrow"
+          />
+        </div>
+        <div className="environment-dropdown__options">
+          {options.map(option => (
+            <div>{option}</div>
+          ))}
+        </div>
       </div>
-      <div className="environment-dropdown__options">
-        {options.map(option => (
-          <div>{option}</div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
