df5c5d14a0c34cba1d11486115410aca998023c8
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-05 20:29:44 +0000

    re-factoring task creation page to accomodate the addition of the buttons section

--- frontend/src/components/CreateTaskPage/CreateTaskPage.js
+++ frontend/src/components/CreateTaskPage/CreateTaskPage.js
@@ -37,68 +37,73 @@ export default class CreateTaskPage extends Component {
   render() {
     return (
       <div className="task-creation">
-        <form className="task-creation__left">
-          <div className="task-creation__left__top">
-            <label className="task-creation__left__top__name">
-              <span>Name</span>
+        <div className="task-creation__top">
+          <form className="task-creation__left">
+            <div className="task-creation__left__top">
+              <label className="task-creation__left__top__name">
+                <span>Name</span>
+                <input
+                  name="taskName"
+                  type="text"
+                  autoComplete="off"
+                  value={this.state.taskName}
+                  onChange={this.handleInputChange}
+                />
+              </label>
+              <div className="task-creation__left__top__severity">
+                <div className="task-creation__left__top__severity__label">
+                  Severity
+                </div>
+                <div className="task-creation__left__top__severity__checkboxes">
+                  <SeverityCheckbox
+                    severity="High"
+                    selectSeverity={this.selectSeverity}
+                    selected={this.state.taskSeverity === "High" ? true : false}
+                  />
+                  <SeverityCheckbox
+                    severity="Medium"
+                    selectSeverity={this.selectSeverity}
+                    selected={
+                      this.state.taskSeverity === "Medium" ? true : false
+                    }
+                  />
+                  <SeverityCheckbox
+                    severity="Low"
+                    selectSeverity={this.selectSeverity}
+                    selected={this.state.taskSeverity === "Low" ? true : false}
+                  />
+                </div>
+              </div>
+            </div>
+            <label className="task-creation__left__summary">
+              <span>Summary</span>
               <input
-                name="taskName"
+                name="taskSummary"
                 type="text"
                 autoComplete="off"
-                value={this.state.taskName}
+                value={this.state.taskSummary}
                 onChange={this.handleInputChange}
               />
             </label>
-            <div className="task-creation__left__top__severity">
-              <div className="task-creation__left__top__severity__label">
-                Severity
-              </div>
-              <div className="task-creation__left__top__severity__checkboxes">
-                <SeverityCheckbox
-                  severity="High"
-                  selectSeverity={this.selectSeverity}
-                  selected={this.state.taskSeverity === "High" ? true : false}
-                />
-                <SeverityCheckbox
-                  severity="Medium"
-                  selectSeverity={this.selectSeverity}
-                  selected={this.state.taskSeverity === "Medium" ? true : false}
-                />
-                <SeverityCheckbox
-                  severity="Low"
-                  selectSeverity={this.selectSeverity}
-                  selected={this.state.taskSeverity === "Low" ? true : false}
-                />
-              </div>
-            </div>
+            <label className="task-creation__left__description">
+              <span>Description</span>
+              <textarea
+                name="taskDescription"
+                type="text"
+                autoComplete="off"
+                value={this.state.taskDescription}
+                onChange={this.handleInputChange}
+              />
+            </label>
+          </form>
+          <div className="task-creation__right">
+            <EnvironmentDropdown
+              selectEnvironment={this.selectEnvironment}
+              environment={this.state.environment}
+            />
           </div>
-          <label className="task-creation__left__summary">
-            <span>Summary</span>
-            <input
-              name="taskSummary"
-              type="text"
-              autoComplete="off"
-              value={this.state.taskSummary}
-              onChange={this.handleInputChange}
-            />
-          </label>
-          <label className="task-creation__left__description">
-            <span>Description</span>
-            <textarea
-              name="taskDescription"
-              type="text"
-              autoComplete="off"
-              value={this.state.taskDescription}
-              onChange={this.handleInputChange}
-            />
-          </label>
-        </form>
-        <div className="task-creation__right">
-          <EnvironmentDropdown
-            selectEnvironment={this.selectEnvironment}
-            environment={this.state.environment}
-          />
         </div>
+
         <div className="task-creation__bottom">
           <TaskButton action="Create" />
         </div>
