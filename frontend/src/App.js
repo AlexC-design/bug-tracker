0c410803a456ad91e1aa320f46c97d44c29b1ea5
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-08 16:43:38 +0000

    adding redux-persist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter, Route } from "react-router-dom";
-import { store } from "./store/configureStore";
+import { store, persistor } from "./store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Navbar from "./components/Navbar";
 import LoginPage from "./components/LoginPage/LoginPage";
@@ -11,10 +12,12 @@ import CreateTaskPage from "./components/CreateTaskPage/CreateTaskPage";
 
 import "./css/app.css";
 
+console.log(persistor);
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
         <HashRouter basename="/">
           <Navbar />
           <Route path="/" exact component={LoginPage} />
@@ -27,6 +30,7 @@ export default class App extends React.Component {
           />
           <div className="app-background" />
         </HashRouter>
+        {/* </PersistGate> */}
       </Provider>
     );
   }
--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,17 +1,30 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import createSagaMiddleware from "redux-saga";
 import thunk from "redux-thunk";
 
-import reducers from "./reducers";
+import rootReducer from "./reducers";
 import rootSaga from "./sagas";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// && persistedReducer
+
 export const store = createStore(
-  reducers(),
+  rootReducer(),
   composeEnhancers(applyMiddleware(sagaMiddleware, thunk))
 );
+
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
