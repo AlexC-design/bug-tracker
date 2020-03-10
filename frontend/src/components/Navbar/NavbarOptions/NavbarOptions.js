d9139f46b5b73359b14f139446c49187f03c22ab
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-10 21:28:53 +0000

    adding links to navbar options

--- frontend/src/components/Navbar/NavbarOptions/NavbarOptions.js
+++ frontend/src/components/Navbar/NavbarOptions/NavbarOptions.js
@@ -1,17 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 import "./css/navbar-options.css";
 
-export const NavbarOptions = ({ isAdmin }) => {
+const NavbarOptions = ({ history, isAdmin, projectId }) => {
+  const switchPage = page => {
+    history.replace(`/project/${projectId}/${page}`);
+  };
+
   return (
     <div className="navbar-options">
       {isAdmin && (
-        <div className="navbar-link navbar-options__members">Members</div>
+        <div
+          onClick={() => switchPage("members")}
+          className="navbar-link navbar-options__members"
+        >
+          Members
+        </div>
       )}
-      <div className="navbar-link navbar-options__created">Created</div>
+      <div
+        onClick={() => switchPage("created")}
+        className="navbar-link navbar-options__created"
+      >
+        Created
+      </div>
       {isAdmin && (
-        <div className="navbar-link navbar-options__unassigned">Unassigned</div>
+        <div
+          onClick={() => switchPage("unassigned")}
+          className="navbar-link navbar-options__unassigned"
+        >
+          Unassigned
+        </div>
       )}
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  projectId: state.selectedProject._id
+});
+
+const wrappedComponent = withRouter(NavbarOptions);
+
+export default connect(mapStateToProps)(wrappedComponent);
