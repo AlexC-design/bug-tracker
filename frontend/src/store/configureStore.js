aa13ae230d35c99468b9c53dab2ccd6796dbca6c
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-14 23:40:52 +0000

    fixing redux persist error

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 import createSagaMiddleware from "redux-saga";
 import thunk from "redux-thunk";
@@ -12,9 +12,9 @@ import rootSaga from "./sagas";
 const persistConfig = {
   key: "root",
   storage,
-  // stateReconciler: autoMergeLevel2
+  stateReconciler: autoMergeLevel2
 };
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer());
 
 const sagaMiddleware = createSagaMiddleware();
 
