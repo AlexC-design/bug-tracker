4f7e40beea10a57426237111ec44be3382ede850
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-15 10:56:10 +0000

    populating member card with data from db

--- frontend/src/components/MembersPage/MemberCard/MemberCard.js
+++ frontend/src/components/MembersPage/MemberCard/MemberCard.js
@@ -1,16 +1,19 @@
 import React from "react";
 import memberIcon from "../../../assets/svg/member-icon.svg";
 import { DeleteButton } from "../../DeleteButton/DeleteButton";
+import { dateFormatting } from "./dateFormatting";
 
 import "./css/member-card.css";
 
-export const MemberCard = () => {
+export const MemberCard = ({ name, date }) => {
+  const formattedDate = dateFormatting(date);
+
   return (
     <div className="member-card">
       <img className="member-card__icon" src={memberIcon} />
       <div className="member-card__details">
-        <div className="member-card__details__name">Member Name</div>
-        <div className="member-card__details__date">Date Joined</div>
+        <div className="member-card__details__name">{name}</div>
+        <div className="member-card__details__date">Joined: {formattedDate}</div>
       </div>
       <DeleteButton />
     </div>
