f5e95f1d401a30e4bed5d58d1037840b41d175a5
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-15 21:15:52 +0000

    replacing snake_casing with camelCasing

--- backend/routes/api/guests.js
+++ backend/routes/api/guests.js
@@ -7,7 +7,7 @@ const Guest = require("../../models/Guest");
 //descr   Create a guest
 router.post("/", (req, res) => {
   const newGuest = new Guest({
-    guest_name: req.body.guestName
+    guestName: req.body.guestName
   });
 
   newGuest.save().then(guest => res.json(guest));
