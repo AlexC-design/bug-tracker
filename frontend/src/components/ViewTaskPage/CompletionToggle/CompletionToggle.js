4a488443daa7ad6fbbc4e8ddbb25a7d8b6b0db65
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-03-24 19:13:51 +0000

    creating completion toggle skeleton

--- frontend/src/components/ViewTaskPage/CompletionToggle/CompletionToggle.js
+++ frontend/src/components/ViewTaskPage/CompletionToggle/CompletionToggle.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const CompletionToggle = () => {
+export const CompletionToggle = ({ taskState }) => {
   return (
     <div className="completion-toggle">
-      <div>CompletionToggle</div>
+      <button
+        className={`completion-toggle__toggle completion-toggle__toggle${
+          taskState ? "--off" : "--on"
+        }`}
+      >
+        <div className="completion-toggle__toggle__button"></div>
+      </button>
+      <div className="completion-toggle__text">
+        {taskState ? "In progress" : "Completed"}
+      </div>
     </div>
   );
 };
