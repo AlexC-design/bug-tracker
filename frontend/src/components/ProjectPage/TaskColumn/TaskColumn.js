135717bba5d180b76b4787434854c875df318f9c
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-04-03 21:05:32 +0100

    task column styling and removing completed section when empty

--- frontend/src/components/ProjectPage/TaskColumn/TaskColumn.js
+++ frontend/src/components/ProjectPage/TaskColumn/TaskColumn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TaskCard from "./TaskCard/TaskCard";
 import SimpleBarReact from "simplebar-react";
 
@@ -6,28 +6,50 @@ import "simplebar/src/simplebar.css";
 import "./css/task-column.css";
 
 export const TaskColumn = ({ priority, tasks }) => {
+  const [noCompleted, setNoCompleted] = useState(true);
+
+  const checkCompleted = (tasks) => {
+    let found = false;
+
+    tasks.map((task) => {
+      if (task.taskCompleted) {
+        found = true;
+      }
+    });
+
+    return found;
+  };
+
+  useEffect(() => {
+    if (checkCompleted(tasks) !== noCompleted) {
+      setNoCompleted(checkCompleted(tasks));
+    }
+  });
+
   return (
     <div className="task-column">
       <div className="task-column__title">{priority}</div>
       {tasks.length ? (
         <SimpleBarReact>
-          {tasks.map(task => {
+          {tasks.map((task) => {
             if (!task.taskCompleted) {
               return <TaskCard taskDetails={task} key={task._id} />;
             } else {
               return null;
             }
           })}
-          <div className="task-column__completed">
-            <div className="task-column__completed__title">Completed</div>
-            {tasks.map(task => {
-              if (task.taskCompleted) {
-                return <TaskCard taskDetails={task} key={task._id} />;
-              } else {
-                return null;
-              }
-            })}
-          </div>
+          {noCompleted && (
+            <div className="task-column__completed">
+              <div className="task-column__completed__title">Completed</div>
+              {tasks.map((task) => {
+                if (task.taskCompleted) {
+                  return <TaskCard taskDetails={task} key={task._id} />;
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+          )}
         </SimpleBarReact>
       ) : (
         <div></div>
