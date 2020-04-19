91c20f97ba9e32f1eccaf2d77ad655a8693b150a
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-04-19 20:51:05 +0100

    setting up heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -16,4 +17,14 @@ app.use("/api/projects", require("./routes/api/projects"));
 app.use("/api/users", require("./routes/api/users"));
 // app.use("/login", require("./routes/login/login"));
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.hml"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => console.log(`Server started on port ${port}`));
