34adb0a8ee0415c7fe5a54ef0bf87b5aec00b0aa
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-04-21 20:40:09 +0100

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const app = express();
 
@@ -17,14 +16,6 @@ app.use("/api/projects", require("./routes/api/projects"));
 app.use("/api/users", require("./routes/api/users"));
 // app.use("/login", require("./routes/login/login"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.hml"));
-  });
-}
-
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
